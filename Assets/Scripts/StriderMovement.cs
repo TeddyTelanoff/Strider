2fb69fea322f1d0b481038961254863857f4a659
Treidex <treidexsemextetry@gmail.com>
2021-03-14 16:54:31 -0700

    Forward wheel correction

--- Assets/Scripts/StriderMovement.cs
+++ Assets/Scripts/StriderMovement.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class StriderMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public Transform m_FrontWheel, m_BackWheel;
+	public float m_CheckDist = 1f, m_CheckMul = 10;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private Vector3 m_FrontWheelBottom, m_BackWheelBottom;
+
+	private void FixedUpdate()
+	{
+		m_FrontWheelBottom = m_FrontWheel.position;
+		m_BackWheelBottom = m_BackWheel.position;
+
+		if (Physics.Raycast(m_FrontWheelBottom, -transform.up, out RaycastHit frontHit, m_CheckDist) && Physics.Raycast(m_BackWheelBottom, -transform.up, out RaycastHit backHit, m_CheckDist))
+		{
+			transform.localRotation = Quaternion.Euler((frontHit.distance - backHit.distance) * m_CheckMul * Vector3.right + transform.localRotation.eulerAngles);
+		}
+	}
+
+	private void OnDrawGizmos()
+	{
+		
+	}
 }
