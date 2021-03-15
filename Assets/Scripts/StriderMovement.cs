786efdc7b83f7e0b327c05e4b9d0671ef904f3be
Treidex <treidexsemextetry@gmail.com>
2021-03-15 13:10:35 -0700

    yay!

--- Assets/Scripts/StriderMovement.cs
+++ Assets/Scripts/StriderMovement.cs
@@ -21,10 +21,10 @@ public class StriderMovement : MonoBehaviour
 	private void FixedUpdate()
 	{
 		float accel = Input.GetAxis("Vertical");
-		m_Rigidbody.AddForce(transform.forward * accel * m_Speed * Time.deltaTime);
+		m_Rigidbody.AddForce(transform.forward * accel * m_Speed * Time.deltaTime, ForceMode.Acceleration);
 
 		float turn = Input.GetAxis("Horizontal");
-		//m_Rigidbody.AddTorque(transform.up * turn * m_TurnSpeed * Time.deltaTime, ForceMode.Force);
-		m_Rigidbody.MoveRotation(Quaternion.Euler(m_Rigidbody.rotation.eulerAngles + transform.up * turn * m_TurnSpeed * Time.deltaTime));
-	}
+        m_Rigidbody.AddTorque(transform.up * turn * m_TurnSpeed * Time.deltaTime, ForceMode.Acceleration);
+        //m_Rigidbody.MoveRotation(Quaternion.Euler(m_Rigidbody.rotation.eulerAngles + transform.up * turn * m_TurnSpeed * Time.deltaTime));
+    }
 }
