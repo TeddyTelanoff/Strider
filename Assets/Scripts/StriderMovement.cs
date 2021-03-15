5808150b95655b337bef79ace43a2b44904bb49d
Treidex <treidexsemextetry@gmail.com>
2021-03-15 15:04:48 -0700

    ...

--- Assets/Scripts/StriderMovement.cs
+++ Assets/Scripts/StriderMovement.cs
@@ -4,27 +4,37 @@ using UnityEngine;
 
 public class StriderMovement : MonoBehaviour
 {
+	private const float c_HitDist = 1;
+
 	[SerializeField]
 	private float m_Speed, m_TurnSpeed;
 
 	private Rigidbody m_Rigidbody;
 
-	private Transform m_Body;
+	[SerializeField]
+	private Transform m_Front, m_Back;
 
 	private void Awake()
 	{
 		m_Rigidbody = GetComponent<Rigidbody>();
-
-		m_Body = transform.Find("Body");
 	}
 
 	private void FixedUpdate()
 	{
-		float accel = Input.GetAxis("Vertical");
+		RaycastHit hitFront, hitBack;
+		bool frontHit = Physics.Raycast(m_Front.position, -m_Front.up, out hitFront, c_HitDist);
+		bool backHit = Physics.Raycast(m_Back.position, -m_Front.up, out hitBack, c_HitDist);
+		if (frontHit || backHit)
+		{
+			float diff = hitFront.distance - hitBack.distance;
+			m_Rigidbody.AddTorque(transform.right * diff * m_TurnSpeed * Time.deltaTime, ForceMode.Impulse);
+		}
+
+		float accel = Input.GetAxisRaw("Vertical");
 		m_Rigidbody.AddForce(transform.forward * accel * m_Speed * Time.deltaTime, ForceMode.Acceleration);
 
-		float turn = Input.GetAxis("Horizontal");
-        m_Rigidbody.AddTorque(transform.up * turn * m_TurnSpeed * Time.deltaTime, ForceMode.Acceleration);
-        //m_Rigidbody.MoveRotation(Quaternion.Euler(m_Rigidbody.rotation.eulerAngles + transform.up * turn * m_TurnSpeed * Time.deltaTime));
-    }
+		float turn = Input.GetAxisRaw("Horizontal");
+		m_Rigidbody.AddTorque(transform.up * turn * m_TurnSpeed * Time.deltaTime, ForceMode.Acceleration);
+		//m_Rigidbody.MoveRotation(Quaternion.Euler(m_Rigidbody.rotation.eulerAngles + transform.up * turn * m_TurnSpeed * Time.deltaTime));
+	}
 }
