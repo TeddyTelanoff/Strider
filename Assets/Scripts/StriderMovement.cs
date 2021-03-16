8ccff505f097945d19402fd044da4e346e380993
Treidex <treidexsemextetry@gmail.com>
2021-03-15 18:10:00 -0700

    hardness

--- Assets/Scripts/StriderMovement.cs
+++ Assets/Scripts/StriderMovement.cs
@@ -4,38 +4,31 @@ using UnityEngine;
 
 public class StriderMovement : MonoBehaviour
 {
-	private const float c_HitDist = 1;
-
 	[SerializeField]
-	private float m_Speed, m_TurnSpeed;
+	private float m_Speed;
+	[SerializeField]
+	private float m_TurnSpeed;
+	[SerializeField]
+	private Transform m_Collisions;
+	[SerializeField]
+	private Transform m_Model;
+	[SerializeField]
+	private float m_ModelMove;
 
 	private Rigidbody m_Rigidbody;
 
-	[SerializeField]
-	private Transform m_Front, m_Back;
-
-	private void Awake()
-	{
+	private void Awake() =>
 		m_Rigidbody = GetComponent<Rigidbody>();
-	}
 
 	private void FixedUpdate()
 	{
-		RaycastHit hitFront, hitBack;
-		bool frontHit = Physics.Raycast(m_Front.position, -m_Front.up, out hitFront, c_HitDist);
-		bool backHit = Physics.Raycast(m_Back.position, -m_Front.up, out hitBack, c_HitDist);
-		if (frontHit || backHit)
-		{
-			float diff = hitFront.distance - hitBack.distance;
-            //m_Rigidbody.AddTorque(transform.right * diff * m_TurnSpeed * Time.deltaTime, ForceMode.Impulse);
-            //m_Rigidbody.MoveRotation(Quaternion.Euler(m_Rigidbody.rotation.eulerAngles + transform.right * diff * Time.deltaTime));
-        }
-
-		float accel = Input.GetAxisRaw("Vertical");
+		float accel = Input.GetAxis("Vertical");
 		m_Rigidbody.AddForce(transform.forward * accel * m_Speed * Time.deltaTime, ForceMode.Acceleration);
 
-		float turn = Input.GetAxisRaw("Horizontal");
-        //m_Rigidbody.AddTorque(transform.up * turn * m_TurnSpeed * Time.deltaTime, ForceMode.Acceleration);
-        m_Rigidbody.MoveRotation(Quaternion.Euler(m_Rigidbody.rotation.eulerAngles + transform.up * turn * m_TurnSpeed * Time.deltaTime));
-    }
+		float turn = Input.GetAxis("Horizontal");
+        m_Rigidbody.AddTorque(transform.up * turn * m_TurnSpeed * Time.deltaTime, ForceMode.Acceleration);
+		//m_Rigidbody.MoveRotation(Quaternion.Euler(m_Rigidbody.rotation.eulerAngles + transform.up * turn * m_TurnSpeed * Time.deltaTime));
+
+		m_Model.localRotation = Quaternion.Euler(new Vector3(m_Collisions.localRotation.eulerAngles.x + m_ModelMove * -turn, m_Collisions.localRotation.eulerAngles.y + m_ModelMove * turn, m_Collisions.localRotation.eulerAngles.z));
+	}
 }
